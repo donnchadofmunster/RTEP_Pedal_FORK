989181449f1abd13a816754c6a3d48ed46cb19db
Djan Gural Tanova <guralcan@gmail.com>
2025-02-26 11:25:21 +0000

    initial samplingmodule implementation

--- code/samplingmodule.cpp
+++ code/samplingmodule.cpp
@@ -1,6 +1,57 @@
-#include "SamplingModule.h"
+#include "samplingmodule.h"
+#include <iostream>
 
-void SamplingModule::captureAudio(float &sample)
+SamplingModule::SamplingModule(unsigned int sampleRate, unsigned int bufferSize)
+    : sampleRate(sampleRate), bufferSize(bufferSize), running(false), handle(nullptr)
 {
-    sample = 0.5f;
+    snd_pcm_open(&handle, "default", SND_PCM_STREAM_CAPTURE, 0);
+    snd_pcm_set_params(handle, SND_PCM_FORMAT_FLOAT_LE, SND_PCM_ACCESS_RW_INTERLEAVED,
+                       1, sampleRate, 1, 500000);
+}
+
+SamplingModule::~SamplingModule()
+{
+    stop();
+    if (handle)
+    {
+        snd_pcm_close(handle);
+    }
+}
+
+void SamplingModule::registerCallback(SampleCallback callback)
+{
+    callbacks.push_back(callback);
+}
+
+void SamplingModule::start()
+{
+    running = true;
+    audioThread = std::thread(&SamplingModule::captureAudio, this);
+}
+
+void SamplingModule::stop()
+{
+    running = false;
+    if (audioThread.joinable())
+    {
+        audioThread.join();
+    }
+}
+
+void SamplingModule::captureAudio()
+{
+    std::vector<float> buffer(bufferSize);
+    while (running)
+    {
+        int err = snd_pcm_readi(handle, buffer.data(), bufferSize);
+        if (err < 0)
+        {
+            snd_pcm_recover(handle, err, 0);
+            continue;
+        }
+        for (const auto &callback : callbacks)
+        {
+            callback(buffer);
+        }
+    }
 }
--- code/samplingmodule.h
+++ code/samplingmodule.h
@@ -1,9 +1,30 @@
 #ifndef SAMPLINGMODULE_H
 #define SAMPLINGMODULE_H
 
+#include <vector>
+#include <functional>
+#include <thread>
+#include <atomic>
+#include <alsa/asoundlib.h>
+
 class SamplingModule
 {
 public:
-    void captureAudio(float &sample);
+    using SampleCallback = std::function<void(const std::vector<float> &)>;
+    SamplingModule(unsigned int sampleRate = 44100, unsigned int bufferSize = 1024);
+    ~SamplingModule();
+    void start();
+    void stop();
+    void registerCallback(SampleCallback callback);
+
+private:
+    void captureAudio();
+    std::thread audioThread;
+    std::atomic<bool> running;
+    snd_pcm_t *handle;
+    unsigned int sampleRate;
+    unsigned int bufferSize;
+    std::vector<SampleCallback> callbacks;
 };
+
 #endif // SAMPLINGMODULE_H
