43b375e9796accbc1459da3e139ccdc1686be5ed
Alex Lake <alexander.lake29@gmail.com>
2025-04-09 14:13:44 +0100

    Fixed init.sh and added CI/CD for all but testing

--- code/init.sh
+++ code/init.sh
@@ -1,13 +1,13 @@
 original_directory=$(pwd)
 script_directory=$(dirname "$0")
 
-apt update
-apt upgrade
-apt install build-essential
-apt install cmake
-apt install clang clang-tidy
-apt install libsndfile1-dev pkg-config
-apt install libasound2-dev
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential
+sudo apt install cmake
+sudo apt install clang clang-tidy
+sudo apt install libsndfile1-dev pkg-config
+sudo apt install libasound2-dev
 
 cd "$script_directory"
 cd "lib"
